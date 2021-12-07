2873e706a0def2626fc7eeff1878803150d04bb4
Grzegorz Staniewicz <grrreg94@gmail.com>
2021-12-07 01:50:18 +0100

    dodana tablica slownikow i test w konsoli czy wiadomosci sa do niej puszowane

--- src/pages/Chat.vue
+++ src/pages/Chat.vue
@@ -4,10 +4,10 @@
     <div class="q-pa-md column col justify-end">
       <q-chat-message
         v-for="message in messages"
-        :key="message.text"
-        :name="message.from"
-        :text="[message.text]"
-        :sent="message.from == 'ja' ? true : false"
+        :key="message.key"
+        :name="message.key"
+        :text="[message.value]"
+        :sent="message.key == 'ja' ? true : false"
       />
     </div>
     <q-footer elevated class="q-pa-sm">
@@ -47,32 +47,85 @@ export default defineComponent({
     return {
       newMessage: '',
       messages: [
-        {
-          text: "Hej",
-          from: "ja",
-        },
-        {
-          text: "Spierdaaj",
-          from: "ktos",
-        },
-        {
-          text: "Dobra",
-          from: "ja",
-        },
+        // {
+        //   text: "Hej",
+        //   from: "ja",
+        // },
+        // {
+        //   text: "Spierdaaj",
+        //   from: "ktos",
+        // },
+        // {
+        //   text: "Dobra",
+        //   from: "ja",
+        // },
       ],
     };
   },
 
   methods: {
     sendMessage() {
-      this.messages.push({
-        text: this.newMessage,
-        from: 'ja'
-      })
+      var dict = new Dictionary();
+      dict.add("ja",this.newMessage);
+      // this.messages.push({
+      //   text: this.newMessage,
+      //   from: 'ja'
+      // })
       this.newMessage = ''
+      console.log(dict.values());
+      console.log(dict.keys());
+      console.log(dict.size());
     }
   }
 });
 
+function Dictionary(){
+  this.data = {};
+  this.add = function(key, value){
+    this.data[key] = value;
+  };
+  this.remove = function(key) {
+    delete this.data[key];
+  };
+  this.has = function(key){
+    return key in this.data;
+  };
+  this.get = function(key) {
+    return this.has(key) ? this.data[key] : undefined;
+  };
+  this.clear = function() {
+    for (var key in this.data) {
+      delete this.data[key];
+    }
+  };
+  this.size = function() {
+    var n = 0;
+    for (var key in this.data) {
+      n++;
+    }
+    return n;
+  };
+  this.keys = function() {
+    var keys = [];
+    for (var key in this.data) {
+      keys.push(key);
+    }
+    return keys;
+  };
+  this.values = function(){
+    var values = [];
+    for (var key in this.data) {
+      if (this.has(key)) {
+        values.push(this.data[key]);
+      }
+    }
+    return values;
+  };
+  this.viewAll = function() {
+    for (var key in this.data) {
+      console.log(key + " -> " + this.data[key]);
+    }
+  };
+}
 
 </script>
